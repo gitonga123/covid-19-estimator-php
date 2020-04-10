292ab1c7263fd24dabc2f126c64c0e47eddeaae1
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2020-04-10 22:21:58 +0300

    Calculate infections By Requested Time && hospital Beds By Requested Time

--- src/estimator.php
+++ src/estimator.php
@@ -1,62 +1,54 @@
 
 <?php
-
 function covid19ImpactEstimator($data)
 {
-
-
-  $new_data = [
-    "data" => $data,
-    'impact' => calculateImpact($data),
-    'severeImpact' => calculateSevereImpact($data)
-  ];
-  return $new_data;
+    $new_data = [
+      "data" => $data,
+      'impact' => calculateImpact(
+          $data,
+          10
+      ),
+      'severeImpact' => calculateImpact(
+          $data,
+          50
+      )
+    ];
+    return $new_data;
 }
 
-function convertJsonToArray($data)
+
+function calculateImpact($data, $reportedCasesMultiplier)
 {
-  return json_decode($data, true);
-}
+    $currentlyInfected = $data['reportedCases'] * $reportedCasesMultiplier;
+    $factor = pow(2, intval(convertPeriodTypeToNumberOfDays($data) / 3));
+    $infectionsByRequestedTime = $currentlyInfected * $factor;
+    $severeCasesByRequestedTime = $infectionsByRequestedTime * 0.15;
+    $hospitalBedsByRequestedTime = (intval($data['totalHospitalBeds'] * 0.35)) - $severeCasesByRequestedTime;
 
-function convertArrayToJson($data)
-{
-  return json_encode($data);
-}
-
-function calculateImpact($data)
-{
-  $currentlyInfected = $data['reportedCases'] * 10;
-  $factor = pow(2, intval(convertPeriodTypeToNumberOfDays($data) / 3));
-  $infectionsByRequestedTime = $currentlyInfected * $factor;
-
-  return ['currentlyInfected' => $currentlyInfected, "infectionsByRequestedTime" => $infectionsByRequestedTime];
-}
-
-function calculateSevereImpact($data)
-{
-  $currentlyInfected = $data['reportedCases'] * 50;
-  $factor = pow(2, intval(convertPeriodTypeToNumberOfDays($data) / 3));
-  $infectionsByRequestedTime = $currentlyInfected * $factor;
-
-  return ['currentlyInfected' => $currentlyInfected, "infectionsByRequestedTime" => $infectionsByRequestedTime];
+    return [
+      'currentlyInfected' => $currentlyInfected,
+      "infectionsByRequestedTime" => $infectionsByRequestedTime,
+      "severeCasesByRequestedTime" => $severeCasesByRequestedTime,
+      "hospitalBedsByRequestedTime" => $hospitalBedsByRequestedTime
+    ];
 }
 
 function convertPeriodTypeToNumberOfDays($data)
 {
-  $periodType = $data['periodType'];
-  $timeToElapse = $data['timeToElapse'];
-  $number_of_days = '';
-  switch ($periodType) {
-    case 'weeks':
-      $number_of_days = $timeToElapse * 7;
-      break;
-    case 'months':
-      $number_of_days = $timeToElapse * 30;
-      break;
-    default:
-      $number_of_days = $timeToElapse;
-      break;
-  }
+    $periodType = $data['periodType'];
+    $timeToElapse = $data['timeToElapse'];
+    $numberOfDays = '';
+    switch ($periodType) {
+        case 'weeks':
+            $numberOfDays = $timeToElapse * 7;
+            break;
+        case 'months':
+            $numberOfDays = $timeToElapse * 30;
+            break;
+        default:
+            $numberOfDays = $timeToElapse;
+            break;
+    }
 
-  return $number_of_days;
+    return $numberOfDays;
 }
